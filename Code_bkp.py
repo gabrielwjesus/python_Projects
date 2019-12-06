601785b4f6f68de791ff159401976e130ff2a803
gabrielwjesus <54075771+gabrielwjesus@users.noreply.github.com>
2019-12-06 17:02:17 -0300

    Create Code_bkp.py

---
+++ Code_bkp.py
@@ -0,0 +1,27 @@
+#This archive call for the type of backup and create log.
+#By Gabriel Wilhelm de Jesus
+
+import time
+
+date =  time.strftime("%Y-%m-%d")
+USER = your_user
+
+def createBkpFull():
+        bkpfile = '%s-bkp-full.tar.gz' % date
+        pathTo = '/mnt/bkp/%s' % bkpfile
+        pathFrom = 'USER@ADDRESS:PATH' #put your user address and path
+        exclude = '*.log, *.temp, .recycle'
+        opts = 'Cravzp'
+        #bkp = 'rsync -%s --exclude={%s} %s %s' %(opts,exclude,pathFrom,pathTo)
+        bkp = 'rsync -%s --rsh="ssh -l %s" --exclude={%s} %s %s' %(opts,USER,exclude,pathFrom,pathTo)
+
+        return bkp
+
+def createBkpInc(): #not ready
+        return True
+
+def createLog():
+        logFile = '%s-bkp-full.txt' % date
+        pathLog = '/var/log/backup-full/%s' % logFile
+
+        return pathLog
