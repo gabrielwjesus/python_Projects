d518eb78a355261213fee96c26b0aea083c6126d
gabrielwjesus <54075771+gabrielwjesus@users.noreply.github.com>
2019-12-06 16:37:41 -0300

    Update Main_bkp.py

--- Main_bkp.py
+++ Main_bkp.py
@@ -1,6 +1,7 @@
 #This is de Main Backup PYTHON script
 #we are going to backup FILES from a FileServer in another BackupServer
 #Incremental Backup is not ready yet
+#Created by Gabriel Wilhelm de Jesus November 2019.
 
 import Code_bkp
 import time
