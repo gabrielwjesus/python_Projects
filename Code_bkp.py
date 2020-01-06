8eb99aeae2f1fb30ee9283b2252d2dc265c22d9f
gabrielwjesus <54075771+gabrielwjesus@users.noreply.github.com>
2020-01-06 14:25:34 -0300

    Update Code_bkp.py

--- Code_bkp.py
+++ Code_bkp.py
@@ -4,24 +4,22 @@
 import time
 
 date =  time.strftime("%Y-%m-%d")
-USER = your_user
 
 def createBkpFull():
         bkpfile = '%s-bkp-full.tar.gz' % date
+        login_user = "USER"
+        server = "SERVER"
         pathTo = '/mnt/bkp/%s' % bkpfile
-        pathFrom = 'USER@ADDRESS:PATH' #put your user address and path
+        pathFrom = 'PATH FROM'
         exclude = '*.log, *.temp, .recycle'
         opts = 'Cravzp'
-        #bkp = 'rsync -%s --exclude={%s} %s %s' %(opts,exclude,pathFrom,pathTo)
-        bkp = 'rsync -%s --exclude={%s} %s %s' %(opts,exclude,pathFrom,pathTo)
+        bkp = 'rsync -%s %s@%s:%s %s' %(opts,login_user,server,pathFrom,pathTo)
 
         return bkp
-
-# def createBkpInc(): #not ready
-#        return True
 
 def createLog():
         logFile = '%s-bkp-full.txt' % date
         pathLog = '/var/log/backup-full/%s' % logFile
 
         return pathLog
+
